a8345e0a291396352976793881491348b3265e74
ycerdan <ycerdan@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-23 11:53:41 +0000

    Big refactoring and cleaning update
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3devapi/trunk@61007 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "t3devapi".
 #
-# Auto generated 24-11-2011 07:48
+# Auto generated 12-04-2012 09:30
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A Powerful API for your (my ?) TYPO3 developments. No manual but the classes are well documented :-)',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.4.18',
+	'version' => '0.4.23',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,9 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"d982";s:28:"class.tx_t3devapi_befunc.php";s:4:"54d5";s:30:"class.tx_t3devapi_calendar.php";s:4:"8191";s:28:"class.tx_t3devapi_config.php";s:4:"5a03";s:30:"class.tx_t3devapi_database.php";s:4:"3349";s:28:"class.tx_t3devapi_export.php";s:4:"69b7";s:27:"class.tx_t3devapi_fluid.php";s:4:"5e59";s:26:"class.tx_t3devapi_html.php";s:4:"e397";s:35:"class.tx_t3devapi_miscellaneous.php";s:4:"150e";s:28:"class.tx_t3devapi_pibase.php";s:4:"28f3";s:32:"class.tx_t3devapi_tagbuilder.php";s:4:"05bd";s:32:"class.tx_t3devapi_templating.php";s:4:"67b3";s:30:"class.tx_t3devapi_validate.php";s:4:"7a32";s:12:"ext_icon.gif";s:4:"3d85";s:17:"ext_localconf.php";s:4:"40a5";s:36:"Tests/Unit/tx_t3devapi_html_Test.php";s:4:"75d2";s:24:"tca/class.tx_tca_map.php";s:4:"7702";}',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"828e";s:28:"class.tx_t3devapi_befunc.php";s:4:"54d5";s:30:"class.tx_t3devapi_calendar.php";s:4:"8191";s:28:"class.tx_t3devapi_config.php";s:4:"5a03";s:30:"class.tx_t3devapi_database.php";s:4:"3349";s:28:"class.tx_t3devapi_export.php";s:4:"69b7";s:27:"class.tx_t3devapi_fluid.php";s:4:"5e59";s:26:"class.tx_t3devapi_html.php";s:4:"85c7";s:35:"class.tx_t3devapi_miscellaneous.php";s:4:"be1f";s:28:"class.tx_t3devapi_pibase.php";s:4:"f4fc";s:30:"class.tx_t3devapi_profiler.php";s:4:"650a";s:32:"class.tx_t3devapi_tagbuilder.php";s:4:"05bd";s:32:"class.tx_t3devapi_templating.php";s:4:"95f4";s:30:"class.tx_t3devapi_validate.php";s:4:"7a32";s:12:"ext_icon.gif";s:4:"3d85";s:17:"ext_localconf.php";s:4:"9d45";s:36:"Tests/Unit/tx_t3devapi_html_Test.php";s:4:"75d2";s:24:"tca/class.tx_tca_map.php";s:4:"7702";}',
 );
 
 ?>
